765788ad0f41f52059d8148d2cdd5f2efcd088c4
doniiali098 <58409808+doniiali098@users.noreply.github.com>
2020-06-11 22:14:03 +0700

    Add files via upload

---
+++ application/views/projek/Template/footer.php
@@ -0,0 +1,16 @@
+<footer style="background-color: #ECF0F0;border-top: 3px solid #22a7a7">
+         <div class="row">    
+            <div class="col-lg-12 col-md-12 col-sm-12">
+              <div class="footer__widget footer__menu">
+                <div class="ft__logo">  
+                  <div style="text-align: center;  padding-top: 10px;color: black"><h style="color:#22a7a7">Patukang.com</h> adalah website penyewaan jasa tukang yang tepat untuk menyelesaikan masalah Anda. Dengan menyediakan banyak jasa tukang sesuai kebutuhan Anda.</div>
+                </div>  
+              <div class="copyright">
+                <div class="copy__right__inner text-left">
+                  <p style="text-align: center">Copyright <i class="fa fa-copyright"></i><a href="https://freethemescloud.com/" style="color: #22a7a7">Free themes Cloud.</a> All Rights Reserved</p>
+                </div>
+              </div>
+              </div>
+            </div>
+        </div>
+      </footer>
---
+++ application/views/projek/Template/head.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Patukang</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Google font (font-family: 'Roboto', sans-serif; Poppins ; Satisfy) -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"> 
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,600,600i,700,700i,800" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+   <link rel="shortcut icon" href="<?php echo base_url('assets/images/patukang.png')?>">
+  
+   <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css')?>"> 
+   <link rel="stylesheet" href="<?php echo base_url('assets/css/plugins.css')?>">
+   <link rel="stylesheet" href="<?php echo base_url('assets/css/uhuy.css')?>">
+   <link rel="stylesheet" href="<?php echo base_url('assets/style.css')?>">
+  <!-- Modernizer js -->
+   <script src="<?php echo base_url('assets/js/vendor/modernizr-3.5.0.min.js')?>"></script>
+   
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/css/bootstrap.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/css/animate.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/css/owl.carousel.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/css/aos.css')?>">
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/css/magnific-popup.css')?>">
+
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/fonts/ionicons/css/ionicons.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/fonts/fontawesome/css/font-awesome.min.css')?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/fonts/flaticon/font/flaticon.css')?>">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/tes/css/style.css')?>">
+
+    <style type="text/css">
+        @media screen and (min-width:700px) and (max-width:3000px){
+        .mobile{display:none;}
+        }
+       @media screen and (max-width:1000px) {
+        .tes{display: none;}
+        }
+         @media screen and (max-width:700px) {
+        .dekstop{display: none;}
+        }
+        
+        .scroll{
+        width: auto;
+        overflow: auto;
+        height: 300px;
+      }
+
+
+
+    .kotakHasil {
+        position: absolute;
+        margin: 0px auto;
+        padding :0px;
+        background-color: white;
+        border : 1px solid #22a7a7;
+    }
+
+    .daftarPencarian {
+        margin: 0px;
+        padding: 0px;
+    }
+
+    .daftarPencarian ul {
+        margin:0px;
+        padding: 0px;
+    }
+
+    .daftarPencarian li {
+        margin:0px;
+        padding: 5px;
+        cursor: pointer;
+        list-style : none;
+    }
+ 
+    .daftarPencarian li:hover {
+        background-color: #22a7a7;
+    }
+
+    #namaDosen{
+        padding : 8px;
+        background-color: white;
+        border : 1px solid #22a7a7;
+        width:100%;
+    }
+
+    #total{
+        background-color:#f3f3f3;
+        padding:10px;
+        text-align:center;
+    }
+
+    #loading{
+        background-color:#f3f3f3;
+        padding:5px;
+        text-align:center;
+    }
+
+    a{
+        text-decoration:none;
+        color:black;
+    }
+
+    a:hover{
+        text-decoration:underline;
+        color:black;
+    }
+    </style>
+</head>
+
+  </html>
